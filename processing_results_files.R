129e2f430020deeb257dbf76fe813f50d55e3c09
Bernard Tembo <4801714+BTembo@users.noreply.github.com>
2019-12-26 11:15:49 +0200

    Amended the code to include correct column names in the data tables.

--- processing_results_files.R
+++ processing_results_files.R
@@ -1,6 +1,6 @@
 
 # Makes the packages below are installed. Run:
-# install.packages(c("stringr", "dplyr", "RSQLite"))
+#install.packages(c("stringr", "dplyr", "RSQLite"))
 
 library(stringr)
 library(dplyr)
@@ -16,6 +16,65 @@ getwd()
 # What database do you want to write your data to?
 database_name <- 'rawdata_combined_ccZambia.db'    #This is where the results will be written to.
 
+# database_name2 <- 'combined_ccZambia_withcolnames.db'    # This is where the results will be written to.
+
+
+###
+
+
+# Function is for adding column names to the data files
+insert_table_headers <- function(fileName, tableInput){
+  
+  tableData <- tableInput
+  
+  if(fileName == "resultsB"){
+    tableData <- tableData %>% 
+      rename(variable = V1, commodity = V2, todrop1 = V3,
+             todrop2 = V4, scenario = V5, climate = V6,
+             year = V7, value = V8)
+      
+      tableData <- tableData %>%  select(-todrop1, -todrop2)
+      
+      } else if(fileName == "resultsA"){
+        tableData <- tableData %>% 
+          rename(variable = V1, scenario = V2, 
+                 climate = V3, year = V4, value = V5)
+        
+      } else if(fileName == "QVAreg"){
+        tableData <- tableData %>% 
+          rename(variable = V1, sector = V2, region = V3,
+                 scenario = V4, climate = V5,
+                 year = V6, value = V7)
+        
+      } else if(fileName == "QFAgg"){
+        tableData <- tableData %>% 
+          rename(variable = V1, labourtype = V2, sector = V3,
+                 scenario = V4, climate = V5,
+                 year = V6, value = V7)
+        
+      } else if(fileName == "HExpend"){
+        tableData <- tableData %>% 
+          rename(variable = V1, household = V2, scenario = V3,
+                 climate = V4, year = V5, value = V6)
+        
+      } else if(fileName == "GDP"){
+        tableData <- tableData %>% 
+          rename(variable = V1, scenario = V2, climate = V3, 
+                 year = V4, value = V5)
+        
+      } else if(fileName == "Agshr"){
+        tableData <- tableData %>% 
+          rename(variable = V1, scenario = V2, climate = V3,
+                 year = V4, value = V5)
+      
+      } else{
+        print("This is a new table!")
+        }
+    
+}
+
+
+###
 
 # Function for collating all the results data.
 get_results_files <- function(getwdInput, dbName){
@@ -48,11 +107,13 @@ get_results_files <- function(getwdInput, dbName){
         
         df_data <- read.csv(file_path, header = F)
         
+        df_data <- insert_table_headers(tableName, df_data)
+        
         # the file path was included for easy auditing purposes.
         df_data <- cbind.data.frame(df_data, filePath = file_path)
         
         df_data <- dplyr::distinct(df_data)
-        # print(dim(df_data))
+        # print((df_data))
         
         dbWriteTable(conn = writeToDB, name = tableName,
                      df_data, append = T, row.names = F)
@@ -77,7 +138,7 @@ get_results_files(wdir, database_name)
 
 
 
-###
+### Only run this code -- below -- if you want to convert the db tables into other file formats.
 
 # Function for converting all database tables into .csv files
 dbTable_to_csv_file <- function(dbName){
